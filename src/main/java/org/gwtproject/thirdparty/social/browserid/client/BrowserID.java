cbcff6e48198860dc0a4eb725d97e49b4f457d37
Kerby Martino <kerbymart@gmail.com>
2015-04-15 10:44:40 +0800

    add login() and logout() functions (not yet working :-( )

--- src/main/java/org/gwtproject/thirdparty/social/browserid/client/BrowserID.java
+++ src/main/java/org/gwtproject/thirdparty/social/browserid/client/BrowserID.java
@@ -38,9 +38,51 @@ public class BrowserID {
         }).setWindow(ScriptInjector.TOP_WINDOW).inject();
     }
 
-    public void init(BrowserIDCallback callback){
+    public static void init(BrowserIDCallback callback){
         BrowserID.callback = callback;
     }
+
+    public static native void logout()/*-{
+        $wnd.navigator.id.logout();
+    }-*/;
+
+    public static void login(BrowserIDCallback callback){
+        BrowserID.callback = callback;
+        _login();
+    }
+
+    private static native void _login()/*-{
+        $wnd.console.log('Logging in...');
+        //var $wnd.currentUser = null;
+        $wnd.navigator.id.get(function (assertion) {
+			if (assertion) {
+			    $wnd.console.log('Login response' + assertion);
+				@org.gwtproject.thirdparty.social.browserid.client.BrowserID::callbackLogin(Lcom/google/gwt/core/client/JavaScriptObject;)(assertion);
+			}else{
+				// error
+				@org.gwtproject.thirdparty.social.browserid.client.BrowserID::callbackFail(Lcom/google/gwt/core/client/JavaScriptObject;)(assertion);
+			}
+		});
+    }-*/;
+
+    private static native void _watch()/*-{
+        $wnd.console.log('Logging in...');
+        //var $wnd.currentUser = null;
+        $wnd.navigator.id.watch({
+            loggedInUser: null,
+            onlogin : function(assertion){
+                $wnd.console.log('Login response: ' + assertion);
+                @org.gwtproject.thirdparty.social.browserid.client.BrowserID::callbackLogin(Lcom/google/gwt/core/client/JavaScriptObject;)(assertion);
+            },
+            onfail : function(response){
+                $wnd.console.log('Fail response: ' + response);
+                @org.gwtproject.thirdparty.social.browserid.client.BrowserID::callbackFail(Lcom/google/gwt/core/client/JavaScriptObject;)(response);
+            },
+            onlogout : function(){
+                @org.gwtproject.thirdparty.social.browserid.client.BrowserID::callbackLogout(Lcom/google/gwt/core/client/JavaScriptObject;)(response);
+            }
+        });
+    }-*/;
 
     public static native void browserID(Element el, String verifier)/*-{
         var options = {
@@ -62,7 +104,7 @@ public class BrowserID {
     }-*/;
 
     @SuppressWarnings("unused")
-    public static void callbackLogin(JavaScriptObject response){
+    public static void callbackLogin(JavaScriptObject assertion){
         BrowserID.callback.onLogin();
     }
 
